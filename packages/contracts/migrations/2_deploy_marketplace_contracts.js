180cfd29f90c3eefb5016082d007d4cb3cc49fd3
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2021-03-28 09:49:02 +0300

    Update 2_deploy_marketplace_contracts.js

--- packages/contracts/migrations/2_deploy_marketplace_contracts.js
+++ packages/contracts/migrations/2_deploy_marketplace_contracts.js
@@ -1,6 +1,6 @@
-const OriginToken = artifacts.require('./token/OriginToken.sol')
+const NordicEnergyToken = artifacts.require('./token/NordicEnergyToken.sol')
 const V01_Marketplace = artifacts.require('./marketplace/V01_Marketplace.sol')
-const MockOUSD = artifacts.require('./token/MockOUSD.sol')
+const MockNUSD = artifacts.require('./token/MockNUSD.sol')
 
 module.exports = function(deployer, network) {
   return deployer.then(() => {
@@ -12,21 +12,21 @@ async function deployContracts(deployer, network) {
   const IS_DEV = network === 'development'
 
   // Initial supply of 1B tokens, in natural units.
-  await deployer.deploy(OriginToken, '1000000000000000000000000000')
+  await deployer.deploy(NordicEnergyToken, '1000000000000000000000000000')
 
-  await deployer.deploy(V01_Marketplace, OriginToken.address)
+  await deployer.deploy(V01_Marketplace, NordicEnergyToken.address)
 
   if (IS_DEV) {
-    await deployer.deploy(MockOUSD, "OriginDollar", "OUSD", "18", "1000000000000000000000000000")
+    await deployer.deploy(MockNUSD, "NordicEnergyDollar", "NUSD", "18", "1000000000000000000000000000")
   }
 
   //register the marketplace as a possible caller upon token approval
-  const token = await OriginToken.deployed()
+  const token = await NordicEnergyToken.deployed()
   const contractOwner = await token.owner()
   await token.addCallSpenderWhitelist(V01_Marketplace.address, {from: contractOwner})
 
   if (IS_DEV) {
-    await MockOUSD.deployed()
+    await MockNUSD.deployed()
   }
 
 }
