63a882259f8757146771d2053c62856bb3dae3da
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2021-03-28 10:53:27 +0300

    Update config.js

--- backend/db/config.js
+++ backend/db/config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 // DB config for migrations only.
 // The config used by the app is under src/models/index.js
 
-const sqlite = `sqlite:${__dirname}/dshop.db`
+const sqlite = `sqlite:${__dirname}/marketplace.db`
 const url = process.env.DATABASE_URL || sqlite
 
 module.exports = {
